1e92b73cc95f58e7e548d84c7f248bf4ea444fb2
unknown <samiizadeh.ali@gmail.com>
2024-01-27 18:44:54 +0330

    experience section developed

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,8 @@ function App() {
           <Hero />
         </div>
         <About />
-        {/* <Experience />
-        <Tech />
+        <Experience />
+        {/* <Tech />
         <Works />
         <FeedBacks /> */}
       </div>
