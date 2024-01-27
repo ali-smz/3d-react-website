05126132f69ee318adeb854ed8d151bf7368c522
unknown <samiizadeh.ali@gmail.com>
2024-01-27 18:00:18 +0330

    about section developed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,22 +3,54 @@ import { styles } from "../styles";
 import { services } from "../constants/index";
 import { fadeIn, textVariant } from "../utils/motion";
 import { motion } from "framer-motion";
+import { sectionWrapper } from "../hoc";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="w-full xs:w-[250px]">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+          options={{ max: 45, scale: 1, speed: 450 }}
+        >
+          <img className="w-16 h-16 object-contain" src={icon} alt={title} />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
+
 const About = () => {
   return (
     <>
-      <motion.div variants={textVariant()} className="mt-[12rem]">
+      <motion.div variants={textVariant()} className="mt-[12rem] ms-10">
         <p className={styles.sectionSubText}>introduction</p>
         <h1 className={styles.sectionHeadText}>Overview.</h1>
       </motion.div>
-      <motion.p variants={fadeIn("", "", 0.1, 1)} className="text-secondary text-[17px] mt-4 max-w-3xl leading-[30px]">
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="text-secondary text-[17px] mt-4 max-w-3xl leading-[30px] ms-10"
+      >
         I'm a skilled software developer with experience in TypeScript and
         JavaScript, and expertise in frameworks like React, Node.js, and
         Three.js. I'm a quick learner and collaborate closely with clients to
         create efficient, scalable, and user-friendly solutions that solve
         real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
+
+      <div className=" mt-20 flex flex-wrap gap-10 items-center justify-center">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
     </>
   );
 };
 
-export default About;
+export default sectionWrapper(About, "about");
