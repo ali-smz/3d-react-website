e59e51d96b2c072d42024198e3a47d2f484fa4a9
unknown <samiizadeh.ali@gmail.com>
2024-02-06 21:11:43 +0330

    read me file

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import {motion} from 'framer-motion'
+import { styles } from '../styles'
+import { sectionWrapper } from '../hoc'
+import { testimonials } from '../constants'
+import { fadeIn, textVariant } from '../utils/motion'
 
 const Feedbacks = () => {
   return (
-    <div>Feedbacks</div>
+    <div className='mt-12 bg-black-100 rounded-[20px]'>
+        <div className={`bg-tertiary rounded-2xl min-h-[300px] ${styles.padding}`}>
+          <motion.div variants={textVariant()}>
+            <p className={styles.sectionSubText}>what others say</p>
+            <h2 className={styles.sectionHeadText}>testimonials.</h2>
+          </motion.div>
+        </div>
+    </div>
   )
 }
 
